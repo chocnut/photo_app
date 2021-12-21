938ea7f05ade19ac8dee133d6470dec956acf476
Peter Indiola <peter@incube8.sg>
2021-12-21 16:18:13 +0800

    Refactoring

--- pages/components/PhotoCard/PhotoCard.tsx
+++ pages/components/PhotoCard/PhotoCard.tsx
@@ -10,29 +10,31 @@ type Props = {
 const PhotoCard = ({ photo }: Props) => {
   return (
     <div className={styles.galleryItem}>
-      <Image
-        className={styles.galleryImage}
-        src={photo.src.portrait}
-        height={500}
-        width={500}
-        alt={photo.alt}
-      />
-      <div className={styles.galleryItemInfo}>
-        <ul>
-          <li
-            data-testid="photographer-name"
-            className={styles.galleryItemPhotographer}
-          >
-            {photo.photographer}
-          </li>
-          <li
-            data-testid="photographer-url"
-            className={styles.galleryItemPhotographerUrl}
-          >
-            {photo.photographer_url}
-          </li>
-        </ul>
-      </div>
+      <a href={photo.url} target="_blank" rel="noreferrer">
+        <Image
+          className={styles.galleryImage}
+          src={photo.src.portrait}
+          height={500}
+          width={500}
+          alt={photo.alt}
+        />
+        <div className={styles.galleryItemInfo}>
+          <ul>
+            <li
+              data-testid="photographer-name"
+              className={styles.galleryItemPhotographer}
+            >
+              {photo.photographer}
+            </li>
+            <li
+              data-testid="photographer-url"
+              className={styles.galleryItemPhotographerUrl}
+            >
+              {photo.photographer_url}
+            </li>
+          </ul>
+        </div>
+      </a>
     </div>
   );
 };
